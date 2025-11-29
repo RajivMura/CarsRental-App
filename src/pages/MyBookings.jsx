2dc565a2d80d5cea8bf5ab270fc850a2fe92492d
RajivMura <rajivmura299@gmail.com>
2025-11-29 16:12:11 +0530

    modified

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyMyBookingsData } from '../assets/assets'
+import Title from '../components/Title'
 
 const MyBookings = () => {
+
+
+  const [bookings, setBookings] = useState([])
+
+  const fetchMyBookings = async () => {
+    setBookings(dummyMyBookingsData)
+  }
+
+  useEffect(() => { 
+    fetchMyBookings()
+  },[])
+
   return (
-    <div>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl'>
+      <Title title='My Bookings' subTitle='View and manage your all car bookings' align='left' />
+      
+      <div>
+        {bookings.map((bookings, index) => (
+          <div key={bookings._id} className='grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor rounded-lg mt-5 first:mt-12'>
+            
+            {/*  Car image + info*/}
+            <div className='md:col-span-1'>
+              <div className='rounded-md overflow-hidden mb-3'>
+                <img src={bookings.car.image} alt="" className='w-full h-auto aspect-video object-cover' />
+
+              
+              </div>
+
+              <p className='text-lg font-medium mt-2'>{bookings.car.brand} {bookings.car.model}</p>
+              <p className='text-gray-500'>{bookings.car.year}  {bookings.car.category}  {bookings.car.location}</p>
+            
+            
+            </div>
+          
+          </div>
+        ))}
+      </div>
       
     </div>
-  )
+  ) 
 }
 
 export default MyBookings
